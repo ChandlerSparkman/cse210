e55933185b56da163faa68487ce26ff5a7f33b5e
ChandlerSparkman <csparkman@byupathway.edu>
2025-11-04 20:50:48 -0600

    Completed Week 1 Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What grade percentage did you earn? ");
+        string grade = Console.ReadLine();
+        int gradenum = int.Parse(grade);
+
+        if (gradenum >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (gradenum >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (gradenum >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (gradenum >= 60)
+        {
+            Console.WriteLine("You got a D.");
+        }
+        else
+        {
+            Console.WriteLine("You got an F.");
+        }
+
+        if (gradenum < 70)
+        {
+            Console.WriteLine("You failed the class. Sorry about that, bud. You got it next time!");
+        }
+        else
+        {
+            Console.WriteLine("You passed! Congrats, I knew you could.");
+        }
     }
 }
