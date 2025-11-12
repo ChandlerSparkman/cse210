d4a2f9fbf316e677c5119279248416d4f659e691
ChandlerSparkman <csparkman@byupathway.edu>
2025-11-12 13:44:51 -0600

    Completed Fractions learning activity

---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,38 @@
+using System.Dynamic;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string printedFraction = $"{_numerator}/{_denominator}";
+        return printedFraction;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decForm = (double)_numerator / (double)_denominator;
+        return decForm;
+    }
+}
