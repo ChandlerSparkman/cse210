8d43b5d0016175e02a9e6e663aeb20c06fe8000b
ChandlerSparkman <csparkman@byupathway.edu>
2025-12-10 16:31:32 -0600

    Completed Shapes Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square(3, "green");
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle(4, 3, "blue");
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle(2.5, "red");
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color: {color}\nArea: {area}\n");
+        }
     }
 }
